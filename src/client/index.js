e59c85f569f1b8f154a988b28d231b1dfa0a2b4d
kkolur5 <99568777+kkolur5@users.noreply.github.com>
2024-04-16 15:39:19 -0400

    Header Component - Most Recent

--- src/client/index.js
+++ src/client/index.js
@@ -3,3 +3,10 @@ import { App } from './App.js';
 // Mount the application to the root element.
 const app = new App();
 await app.render('root');
+
+import { Header } from './Header.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const header = new Header('team-friendship/assets/logo.png', 'KeyMate');
+  document.body.insertBefore(header.render(), document.body.firstChild);
+});
