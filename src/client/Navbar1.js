bda32bf61a16e1e481607ec87fae4294c390ba93
Ashley Bhandari <ashleybhanda@umass.edu>
2024-04-16 18:02:38 -0400

    Milestone 2 - Navbar2 (logged in)

---
+++ src/client/Navbar1.js
@@ -0,0 +1,27 @@
+import { Events } from './Events.js';
+
+export class Navbar1 {
+    #events = null;
+
+    constructor() {
+        this.#events = Events.events();
+    }
+    
+    async render() {
+        const elm = document.createElement('nav');
+        elm.classList.add('navbar1');
+
+        elm
+            .querySelectorAll('a')
+            .forEach(link =>
+                link.addEventListener('click', async (e) => {
+                    e.preventDefault();
+                    const view = e.target.getAttribute('href').replace('#', '');
+                    window.location.hash = view;
+                    await this.#events.publish('navigateTo', view);
+                })
+            );
+        
+        return elm;
+    }
+}
---
+++ src/client/Navbar2.js
@@ -0,0 +1,42 @@
+import { Events } from './Events.js';
+
+export class Navbar2 {
+    #events = null;
+
+    constructor() {
+        this.#events = Events.events();
+    }
+    
+    async render() {
+        const elm = document.createElement('div');
+        elm.classList.add('navbar2');
+
+        elm.innerHTML = `
+        <nav>
+            <div class="text-links">
+                <a href="#discover">Discover</a>
+                <a href="#matches">Matches</a>
+            </div>
+            <div class="settings">
+                <a href="#settings">
+                    <i class="material-symbols-outlined">settings</i>
+                </a>
+            </div>
+        </nav>
+        <hr>
+        `;
+
+        elm
+            .querySelectorAll('a')
+            .forEach(link =>
+                link.addEventListener('click', async (e) => {
+                    e.preventDefault();
+                    const view = e.currentTarget.getAttribute('href').replace('#', '');
+                    window.location.hash = view;
+                    await this.#events.publish('navigateTo', view);
+                })
+            );
+        
+        return elm;
+    }
+}
