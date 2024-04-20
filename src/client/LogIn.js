44c39eea1109f564d7cb05cb285e8b95d4601535
rachlahav <139070344+rachlahav@users.noreply.github.com>
2024-04-20 19:34:54 -0400

    Login.js

---
+++ src/client/LogIn.js
@@ -0,0 +1,31 @@
+import { User} from './DataStructures.js';
+
+let user = new User;
+
+//Change to work with pouchdb
+class AuthService {
+    static async authenticate(username, password) {
+        return new Promise((resolve, reject) => {
+        
+            setTimeout(() => {
+                user = users.find(user => user.email === username && user.password === password);
+                if (user) {
+                    resolve(user); 
+                } else {
+                    reject(new Error('Invalid username or password'));
+                }
+            });
+        });
+    }
+}
+
+async function login(username, password) {
+    try {
+        const user = await AuthService.authenticate(username, password);
+        console.log('Authentication successful:', user);
+        //Change window
+        window.location.href = 'DiscoverHousing.js'; 
+    } catch (error) {
+        console.error('Authentication failed:', error.message);
+    }
+}
