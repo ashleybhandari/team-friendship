03e4092513b82a9c05e941c7c742b84230adfe56
rachlahav <139070344+rachlahav@users.noreply.github.com>
2024-04-20 19:43:46 -0400

    Update AboutView.js

--- src/client/views/AboutView.js
+++ src/client/views/AboutView.js
@@ -1,3 +1,6 @@
+import { Header } from './components/Header.js';
+import { Footer } from './components/Footer.js';
+
 export class AboutView {
     async render() {
         const aboutView = document.createElement('div');
