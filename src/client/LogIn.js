0dec2becf5b3a661e11461537dc57e46e0fc9b68
rachlahav <139070344+rachlahav@users.noreply.github.com>
2024-04-21 10:51:19 -0400

    Update LogIn.js

--- src/client/LogIn.js
+++ src/client/LogIn.js
@@ -1,31 +1,11 @@
-import { User} from './DataStructures.js';
+import dataService from './dataService.js';
 
-let user = new User;
-
-//Change to work with pouchdb
-class AuthService {
-    static async authenticate(username, password) {
-        return new Promise((resolve, reject) => {
-        
-            setTimeout(() => {
-                user = users.find(user => user.email === username && user.password === password);
-                if (user) {
-                    resolve(user); 
-                } else {
-                    reject(new Error('Invalid username or password'));
-                }
-            });
-        });
-    }
-}
-
-async function login(username, password) {
-    try {
-        const user = await AuthService.authenticate(username, password);
-        console.log('Authentication successful:', user);
-        //Change window
-        window.location.href = 'DiscoverHousing.js'; 
-    } catch (error) {
-        console.error('Authentication failed:', error.message);
-    }
+async function login(email, password) {
+  try {
+    const user = await dataService.authenticateUser(email, password);
+    console.log('Authentication successful:', user);
+   //TODO: Change the window
+  } catch (error) {
+    console.error('Authentication failed:', error.message);
+  }
 }
