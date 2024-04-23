1b8f18b0c59e7449352b6f1561dcbc54b202326a
kkolur5 <99568777+kkolur5@users.noreply.github.com>
2024-04-23 15:01:14 -0400

    Update HousingSituationView and css

--- src/client/scripts/views/CreateAccount/HousingSituationView.js
+++ src/client/scripts/views/CreateAccount/HousingSituationView.js
@@ -16,13 +16,19 @@ export class HousingSituationView {
         
         // progress bar
         housingViewElm.appendChild(await new ProgressBar(3).render());
+
+         // Title
+         const titleOptionsContainer = document.createElement('div');
+         titleOptionsContainer.classList.add('title-options-container');
         
-        // Render the radio input for housing situation choices
-        const radioInputElement = await new RadioInput(
+         // Render the radio input for housing situation choices
+         const radioInputElement = await new RadioInput(
             'What is your housing situation?', 
             ['I am looking for housing', 'I have housing and am looking for roommates']
         ).render();
-        housingViewElm.appendChild(radioInputElement);
+        titleOptionsContainer.appendChild(radioInputElement);;
+
+        housingViewElm.appendChild(titleOptionsContainer);
 
         housingViewElm.appendChild(await new Navigation('create-2', 'create-4').render());
 
