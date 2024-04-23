7059249d439e8e8e8b0815ee22848e5fed17821c
rachlahav <139070344+rachlahav@users.noreply.github.com>
2024-04-23 11:54:22 -0400

    Update AboutView.js

--- src/client/scripts/views/SignedOut/AboutView.js
+++ src/client/scripts/views/SignedOut/AboutView.js
@@ -24,7 +24,7 @@ export class AboutView {
                 <div id="email-button-container"></div>
             </section>
 
-            <section id="mission">
+            <section id="mission" style="text-align: center;">
                 <h2>Our Mission</h2>
                 <p>In 2023, UMass announced that it would no longer be able to guarantee on-campus housing for all undergraduates who requested it. The room selection process ended early, leaving 900 students without housing for the fall. These students rushed to the UMass Off Campus Housing website, Facebook Marketplace, Instagram, Discord, and more to blindly message usernames in hopes of coming upon compatible housing and roommates.</p>
                 <p>KeyMate was born to ease this effort by centralizing the search process and presenting students with important data upfront. Akin to a dating app, each user creates a profile in which they describe their traits, habits, and preferences surrounding roommates and housing. KeyMate uses this information to tailor each user's feed. This way, users can tell whether they like each other at a glance; if two users are compatible and interested in each other, they can then safely message within the web application before exchanging contact information.</p>
