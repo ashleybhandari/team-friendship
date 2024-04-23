7e5f973a3f88e5a02d4124ce60675cf82e294aaf
rachlahav <139070344+rachlahav@users.noreply.github.com>
2024-04-23 16:48:44 -0400

    Update LogIn.js

--- src/client/data/LogIn.js
+++ src/client/data/LogIn.js
@@ -1,6 +1,6 @@
 // DB TODO: uncomment
 const PouchDB = require('pouchdb');
-import '*' as PouchDBAuthentication from 'pouchdb-authentication';
+import PouchDBAuthentication from 'pouchdb-authentication';
 
 PouchDB.plugin(PouchDBAuthentication);
 
