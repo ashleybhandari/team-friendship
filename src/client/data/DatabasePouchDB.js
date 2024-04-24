cf902d5bdc480edfa22cdf90596bb77f451fa641
rachlahav <139070344+rachlahav@users.noreply.github.com>
2024-04-24 13:09:48 -0400

    Update DatabasePouchDB.js

--- src/client/data/DatabasePouchDB.js
+++ src/client/data/DatabasePouchDB.js
@@ -2,155 +2,154 @@
 //const PouchDB = require('pouchdb');
 const db = new PouchDB('roommate-matching');
 
-  getAllUsers: async () => {
-    return db.allDocs({ include_docs: true })
-      .then(result => result.rows.map(row => row.doc));
-  }
+const getAllUsers = async () => {
+  return db.allDocs({ include_docs: true })
+    .then(result => result.rows.map(row => row.doc));
+}
 
-  getUserById: async (id) => {
-    return db.get(id);
-  }
+const getUserById = async (id) => {
+  return db.get(id);
+}
 
-  addUser: async (user) => {
-    const newUser = {
-      _id: user.id, // Use the user's id as the document _id
-      email: user.email,
-      avatar: user.avatar,
-      name: user.name,
-      age: user.age,
-      gender: user.gender,
-      character: user.character,
-      education: user.education,
-      socials: user.socials,
-      description: user.description,
-      hasHousing: user.hasHousing,
-      preferences: user.preferences,
-      housing: user.housing,
-      liked: user.liked,
-      rejected: user.rejected,
-      matches: user.matches
-    };
+const addUser = async (user) => {
+  const newUser = {
+    _id: user.id, // Use the user's id as the document _id
+    email: user.email,
+    avatar: user.avatar,
+    name: user.name,
+    age: user.age,
+    gender: user.gender,
+    character: user.character,
+    education: user.education,
+    socials: user.socials,
+    description: user.description,
+    hasHousing: user.hasHousing,
+    preferences: user.preferences,
+    housing: user.housing,
+    liked: user.liked,
+    rejected: user.rejected,
+    matches: user.matches
+  };
 
-    return db.put(newUser);
-  }
+  return db.put(newUser);
+}
 
-  updateUser: async (user) => {
-    const updatedUser = {
-      _id: user.id,
-      _rev: user._rev, // Include the _rev property for updates
-      email: user.email,
-      avatar: user.avatar,
-      name: user.name,
-      age: user.age,
-      gender: user.gender,
-      character: user.character,
-      education: user.education,
-      socials: user.socials,
-      description: user.description,
-      hasHousing: user.hasHousing,
-      preferences: user.preferences,
-      housing: user.housing,
-      liked: user.liked,
-      rejected: user.rejected,
-      matches: user.matches
-    };
-    return db.put(updatedUser);
-  }
+const updateUser = async (user) => {
+  const updatedUser = {
+    _id: user.id,
+    _rev: user._rev, // Include the _rev property for updates
+    email: user.email,
+    avatar: user.avatar,
+    name: user.name,
+    age: user.age,
+    gender: user.gender,
+    character: user.character,
+    education: user.education,
+    socials: user.socials,
+    description: user.description,
+    hasHousing: user.hasHousing,
+    preferences: user.preferences,
+    housing: user.housing,
+    liked: user.liked,
+    rejected: user.rejected,
+    matches: user.matches
+  };
+  return db.put(updatedUser);
+}
 
-  deleteUser: async (id) => {
-    return db.get(id)
-      .then(doc => db.remove(doc));
-  }
+const deleteUser = async (id) => {
+  return db.get(id)
+    .then(doc => db.remove(doc));
+}
 
-   getMatches: async (id) => {
-    const user = user.getUserById(id);
-    return user.matches;
-  }
+const getMatches = async (id) => {
+  const user = await getUserById(id);
+  return user.matches;
+}
 
-  removeMatches: async (currUserId, removeUserId) => {
-    const user = user.getUserById(id);
-    const removeUserIndex = user.matches.indexOf(removeUserId);
-    user.matches = user.matches.splice(removeUserIndex, 1);
-    updateUser(user);
-  } 
-  
-  deleteMatch: async(id, matchId) => {} // TODO
+const removeMatches = async (currUserId, removeUserId) => {
+  const user = await getUserById(currUserId);
+  const removeUserIndex = user.matches.indexOf(removeUserId);
+  user.matches.splice(removeUserIndex, 1);
+  await updateUser(user);
+}
 
-  getAllHousings: async () => {
-    return db.allDocs({ include_docs: true, startkey: 'housing_' })
-      .then(result => result.rows.map(row => row.doc));
-  }
+const deleteMatch = async (id, matchId) => {} // TODO
 
-  getHousingById: async (id) => {
-    return db.get(`housing_${id}`);
-  }
+const getAllHousings = async () => {
+  return db.allDocs({ include_docs: true, startkey: 'housing_' })
+    .then(result => result.rows.map(row => row.doc));
+}
 
-  addHousing: async (housing) => {
-    const newHousing = {
-      _id: `housing_${housing.id}`, // Use a prefix to distinguish housing documents
-      city: housing.city,
-      rent: housing.rent,
-      beds: housing.beds,
-      baths: housing.baths,
-      gender: housing.gender,
-      utilities: housing.utilities,
-      leaseLength: housing.leaseLength,
-      leaseType: housing.leaseType,
-      roomType: housing.roomType,
-      buildingType: housing.buildingType,
-      timeframe: housing.timeframe,
-      amenities: housing.amenities,
-      pics: housing.pics,
-      notes: housing.notes
-    };
+const getHousingById = async (id) => {
+  return db.get(`housing_${id}`);
+}
 
-    return db.put(newHousing);
-  }
+const addHousing = async (housing) => {
+  const newHousing = {
+    _id: `housing_${housing.id}`, // Use a prefix to distinguish housing documents
+    city: housing.city,
+    rent: housing.rent,
+    beds: housing.beds,
+    baths: housing.baths,
+    gender: housing.gender,
+    utilities: housing.utilities,
+    leaseLength: housing.leaseLength,
+    leaseType: housing.leaseType,
+    roomType: housing.roomType,
+    buildingType: housing.buildingType,
+    timeframe: housing.timeframe,
+    amenities: housing.amenities,
+    pics: housing.pics,
+    notes: housing.notes
+  };
 
-  updateHousing: async (housing) => {
-    const updatedHousing = {
-      _id: `housing_${housing.id}`,
-      _rev: housing._rev, // Include the _rev property for updates
-      city: housing.city,
-      rent: housing.rent,
-      beds: housing.beds,
-      baths: housing.baths,
-      gender: housing.gender,
-      utilities: housing.utilities,
-      leaseLength: housing.leaseLength,
-      leaseType: housing.leaseType,
-      roomType: housing.roomType,
-      buildingType: housing.buildingType,
-      timeframe: housing.timeframe,
-      amenities: housing.amenities,
-      pics: housing.pics,
-      notes: housing.notes
-    };
+  return db.put(newHousing);
+}
 
-    return db.put(updatedHousing);
-  }
+const updateHousing = async (housing) => {
+  const updatedHousing = {
+    _id: `housing_${housing.id}`,
+    _rev: housing._rev, // Include the _rev property for updates
+    city: housing.city,
+    rent: housing.rent,
+    beds: housing.beds,
+    baths: housing.baths,
+    gender: housing.gender,
+    utilities: housing.utilities,
+    leaseLength: housing.leaseLength,
+    leaseType: housing.leaseType,
+    roomType: housing.roomType,
+    buildingType: housing.buildingType,
+    timeframe: housing.timeframe,
+    amenities: housing.amenities,
+    pics: housing.pics,
+    notes: housing.notes
+  };
 
-  deleteHousing: async (id) => {
-    return db.get(`housing_${id}`)
-      .then(doc => db.remove(doc));
-  }
+  return db.put(updatedHousing);
+}
 
-  authenticateUser: async (email, password) => {
-    try {
-      const user = await db.get(`user_${email}`);
-      if (user.password === password) {
-        return user;
-      } else {
-        throw new Error('Invalid username or password');
-      }
-    } catch (error) {
-      if (error.status === 404) {
-        throw new Error('Invalid username or password');
-      } else {
-        throw error;
-      }
+const deleteHousing = async (id) => {
+  return db.get(`housing_${id}`)
+    .then(doc => db.remove(doc));
+}
+
+const authenticateUser = async (email, password) => {
+  try {
+    const user = await db.get(`user_${email}`);
+    if (user.password === password) {
+      return user;
+    } else {
+      throw new Error('Invalid username or password');
+    }
+  } catch (error) {
+    if (error.status === 404) {
+      throw new Error('Invalid username or password');
+    } else {
+      throw error;
     }
   }
+}
 
-export {getAllUsers, getUserById, addUser, updateUser, deleteUser, getMatches, removeMatches, deleteMatch, getAllHousings,
-        getHousingById, addHousing, updateHousing, deleteHousing, authenticateUser };
+export { getAllUsers, getUserById, addUser, updateUser, deleteUser, getMatches, removeMatches, deleteMatch, getAllHousings, getHousingById, addHousing, updateHousing, deleteHousing, authenticateUser };
