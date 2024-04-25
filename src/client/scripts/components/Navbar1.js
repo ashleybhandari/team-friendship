65cb6221a8fbb54cf31019919fb4572c6cd77f00
Ashley Bhandari <ashleybhanda@umass.edu>
2024-04-25 06:45:26 -0400

    Added link to Navbar logo, fixed about padding

--- src/client/scripts/components/Navbar1.js
+++ src/client/scripts/components/Navbar1.js
@@ -16,9 +16,11 @@ export class Navbar1 {
         elm.classList.add('navbar1');
 
         elm.innerHTML = `
-        <div class="logo">
-            <img src="https://raw.githubusercontent.com/ashleybhandari/team-friendship/main/assets/logo.png">
-            <h2 class="battambang">KeyMate</h2>
+        <div class="links">
+            <div class="logo">
+                <img src="https://raw.githubusercontent.com/ashleybhandari/team-friendship/main/assets/logo.png">
+                <h2 class="battambang">KeyMate</h2>
+            </div>
             <div class="links">
                 <a href="#landing" id="nav-landing">Home</a>
                 <a href="#about" id="nav-about">About us</a>
@@ -36,20 +38,19 @@ export class Navbar1 {
                 link.addEventListener('click', async (e) => {
                     e.preventDefault();
                     const view = e.target.getAttribute('href').replace('#', '');
-                    this.#navigate(view);
+                    window.location.hash = view;
+                    await this.#events.publish('navigateTo', view);
                 })
             );
+
+        // clicking the logo takes you to the landing
+        elm.querySelector('.logo').addEventListener('click', async (e) => {
+            e.preventDefault();
+            const view = 'landing';
+            window.location.hash = view;
+            await this.#events.publish('navigateTo', view);
+        })
         
         return elm;
-    }
-
-    /**
-     * Navigates to v. If the user is already signed in, clicking the Sign in
-     * button redirects them to the Discover page.
-     * @param {string} view - View to navigate to
-     */
-    async #navigate(view) {
-        window.location.hash = view;
-        await this.#events.publish('navigateTo', view);
     }
 }
