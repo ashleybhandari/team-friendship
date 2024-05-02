71a2b8d32887d9365abf1e4a7b8e17135b83cdab
rachlahav <139070344+rachlahav@users.noreply.github.com>
2024-05-02 16:45:40 -0400

    Update index.js

--- src/client/scripts/index.js
+++ src/client/scripts/index.js
@@ -1,5 +1,5 @@
 import { App } from './App.js';
-var db = new PouchDB('my_database');
+export var db = new PouchDB('my_database');
 
 // Mount the application to the root element.
 const app = new App();
