4df9b62ffbf889444ab3dc857f7305c09127747b
rachlahav <139070344+rachlahav@users.noreply.github.com>
2024-05-01 22:51:57 -0400

    Update DatabasePouchDB.js

--- src/client/data/DatabasePouchDB.js
+++ src/client/data/DatabasePouchDB.js
@@ -2,9 +2,6 @@
 import {PouchDB} from "pouchdb";
 const db = new PouchDB('roommate-matching');
 
-import find from 'pouchdb-find'; // 
-PouchDB.plugin(find); 
-
 export const getAllUsers = async () => {
   return db.allDocs({ include_docs: true })
     .then(result => result.rows.map(row => row.doc));
