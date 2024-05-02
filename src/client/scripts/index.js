9e0373c2a7df9cb3ba7c224588a582acd658cfd8
rachlahav <139070344+rachlahav@users.noreply.github.com>
2024-05-02 17:01:34 -0400

    Update index.js

--- src/client/scripts/index.js
+++ src/client/scripts/index.js
@@ -1,8 +1,4 @@
 import { App } from './App.js';
-const PouchDB = require('pouchdb');
-// Function to create and return a PouchDB instance
-module.exports = db;
-
 
 // Mount the application to the root element.
 const app = new App();
