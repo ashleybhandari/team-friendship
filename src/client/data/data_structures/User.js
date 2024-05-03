70a22faac19aea487909873f20ee8d142ba44c93
rachlahav <139070344+rachlahav@users.noreply.github.com>
2024-05-03 01:05:12 -0400

    Update User.js

--- src/client/data/data_structures/User.js
+++ src/client/data/data_structures/User.js
@@ -38,7 +38,7 @@ export class User {
         socials, description, hasHousing, preferences, housing, liked,
         rejected, matches
     ) {
-        this.id = id ? id.toString();
+        this.id = id;
         this.email = email;
         this.avatar = avatar;
         this.name = name;
