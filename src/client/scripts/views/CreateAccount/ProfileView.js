d2c6517150b9201acdb640850340834eeb0ab92a
Ashley Bhandari <ashleybhanda@umass.edu>
2024-05-07 02:05:26 -0400

    ProfileView validation

--- src/client/scripts/views/CreateAccount/ProfileView.js
+++ src/client/scripts/views/CreateAccount/ProfileView.js
@@ -10,6 +10,8 @@ import { getUserById, updateUser } from '../../../data/DatabasePouchDB.js';
  * view: 'create-2'
  */
 export class ProfileView {
+    #profileViewElm = null;
+    #userProfile = null;
     #database = null;
     #events = null;
     #userId = null;
@@ -19,7 +21,6 @@ export class ProfileView {
      */
     constructor() {
         this.#database = { updateUser };
-
         this.#events = Events.events();
         this.#events.subscribe('createUser', (id) => this.#userId = id);
     }
@@ -30,11 +31,11 @@ export class ProfileView {
      * @returns {Promise<HTMLDivElement>} - A promise that resolves with the rendered ProfileView element.
      */
     async render() {
-        const profileViewElm = document.createElement('div');
-        profileViewElm.id = 'profileView';
+        this.#profileViewElm = document.createElement('div');
+        this.#profileViewElm.id = 'profileView';
 
         // progress bar
-        profileViewElm.appendChild(await new ProgressBar(2).render());
+        this.#profileViewElm.appendChild(await new ProgressBar(2).render());
 
         // header
         const header = document.createElement('div');
@@ -43,19 +44,35 @@ export class ProfileView {
         <h1 class="battambang">Tell us about yourself</h1>
         <p>starred fields are required</p>
         `;
-        profileViewElm.appendChild(header);
+        this.#profileViewElm.appendChild(header);
 
         // page content
         const form = document.createElement('form');
-        form.appendChild(await new UserProfile('profile').render());
-        profileViewElm.appendChild(form);
+        this.#userProfile = new UserProfile('profile');
+        form.appendChild(await this.#userProfile.render());
+        this.#profileViewElm.appendChild(form);
 
         // navigation
-        profileViewElm.appendChild(await new Navigation(
+        this.#profileViewElm.appendChild(await new Navigation(
             'create-1', 'create-3', this.#nextBtnHandlers(form)
         ).render());
         
-        return profileViewElm;
+        return this.#profileViewElm;
+    }
+
+    #isInvalid() {
+        const invalid = this.#userProfile
+            .getRequiredIds('profile')
+            .some((id) => {
+                const elm = this.#profileViewElm.querySelector('#' + id);
+                return elm ? !elm.checkValidity() : false;
+            });
+
+        if (invalid) {
+            alert('Make sure all required fields are filled out (the starred ones)!');
+        }
+
+        return invalid;
     }
 
     /**
@@ -66,9 +83,12 @@ export class ProfileView {
     #nextBtnHandlers(form) {
         // submit form data
         const submitForm = async () => {
+            if (this.#isInvalid()) {
+                throw new Error('Required field(s) empty');
+            }
+
             const formData = new FormData(form);
             const userData = Object.fromEntries(formData.entries());
-            let success = true;
 
             try {
                 const currentUser = await getUserById(userData.id);
