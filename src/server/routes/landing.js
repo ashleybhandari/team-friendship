1be922f947ab6f252ae766d443e392724eb3a510
rachlahav <139070344+rachlahav@users.noreply.github.com>
2024-05-07 16:20:20 -0400

    Documentation

--- src/server/routes/landing.js
+++ src/server/routes/landing.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const router = express.Router();
 
+/**
+ * Defines the routes for the Landing Page.
+ */
+
 router.get('/', (req, res) => {
     res.send('Welcome to the Landing Page!');
 });
