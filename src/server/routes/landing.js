cd50deb8c168d7a0287377f424ec56de11f5921f
rachlahav <139070344+rachlahav@users.noreply.github.com>
2024-05-09 13:13:33 -0400

    Update landing.js

--- src/server/routes/landing.js
+++ src/server/routes/landing.js
@@ -1,7 +1,6 @@
 import express from 'express';
 const router = express.Router();
 import { LandingView } from './views/LandingView.js';
-export default LandingView;
 
 /**
  * Defines the routes for the Landing Page.
@@ -11,4 +10,4 @@ router.get('/', (req, res) => {
     res.send('Welcome to the Landing Page!');
 });
 
-export default router;
+export { LandingView, router };
