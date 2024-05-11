0dbe77e6384c56f6efb50ee6461a274132441deb
Ashley Bhandari <ashleybhanda@umass.edu>
2024-05-11 00:31:29 -0400

    Updated MockData

--- src/client/data/MockData.js
+++ src/client/data/MockData.js
@@ -20,19 +20,19 @@ const u0 = new User(
     { clean: 1, sleep: 2, noise: 2, guests: 1 },
     { major: 'Culinary Arts', school: 'Umass Amherst' },
     {},
-    'Bob. Vestibulum eu facilisis risus. Integer id arcu sodales, finibus nibh nec, consequat ante. Curabitur ut arcu sollicitudin, suscipit mauris id, molestie dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sit amet nisi fermentum, fringilla enim a, aliquam lacus.',
+    `I am a third-generation restaurateur. I run Bob's Burgers with the help of my wife, Linda Belcher, and my three kids, Tina, Gene, and Louise Belcher. While poor with business management and cursed with an unlucky streak, my experience and skill in homemade cuisine have helped my restaurant stay afloat.`,
     false, // hasHousing
     new Preferences(
-        ['Amherst', 'Sunderland', 'Northampton'],
-        { min: 600, max: 1000 },
-        {},
-        { allFemale: false, allMale: true, mixed: true },
-        { semester: true, year: true },
-        { rent: true },
-        { private: true },
-        { apartment: true, house: true },
-        { fall: true },
-        { laundry: true, parking: true }
+        null,
+        null,
+        null,
+        { allFemale: true, allMale: true, mixed: true },
+        { perSemester: true, monthly: true, sixMonths: true, yearly: true },
+        { rent: true, sublet: true },
+        { private: true, shared: true },
+        { dorm: true, apartment: true, house: true },
+        { fall: true, winter: true, spring: true, summer: true },
+        { aircon: true, dishwasher: true, hardwood: true, carpet: true, laundry: true, parking: true, bus: true, pets: true }
     ),
     new Housing(),
     [], // liked
@@ -52,7 +52,7 @@ const u1 = new User(
     { clean: 2, sleep: 3, noise: 3, guests: 2 },
     { major: 'Music', school: 'Smith', level: 'grad' },
     { fb: 'lindabelcher', ig: 'linlig' },
-    'Linda. Vestibulum eu facilisis risus. Integer id arcu sodales, finibus nibh nec, consequat ante. Curabitur ut arcu sollicitudin, suscipit mauris id, molestie dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sit amet nisi fermentum, fringilla enim a, aliquam lacus.',
+    `I have a broad sense of humor, constantly joking with my family and friends. I am wise and kind to my family and love them very much. I co-own Bob's Burgers with my husband and help run the restaurant for him when he's busy or away.`,
     true, // hasHousing
     new Preferences(),
     new Housing(
@@ -69,7 +69,7 @@ const u1 = new User(
         'summer',
         { dishwasher: true, hardwood: true, parking: true, bus: true },
         [ 'https://static.wikia.nocookie.net/bobsburgerpedia/images/3/31/Jimmypestospizza.jpg/revision/latest?cb=20110902183814', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3RIJfoiz3klpLTVdeLL77AT8rMqL1m1gpV8Aor-Pl4Q&s'],
-        'Linda housing notes. Vestibulum eu facilisis risus. Integer id arcu sodales, finibus nibh nec, consequat ante. Curabitur ut arcu sollicitudin, suscipit mauris id, molestie dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sit amet nisi fermentum, fringilla enim a, aliquam lacus.'
+        `Bob's Burgers is the restaurant owned by me and Bob Belcher, which is "conveniently located on Ocean Avenue" (Easy Com-mercial, Easy Go-mercial) in Seymour's Bay, New Jersey. It is located in a rental property that also contains my family's apartment.`
     ),
     [], // liked
     [], // rejected
@@ -88,19 +88,19 @@ const u2 = new User(
     { clean: 3, sleep: 1, noise: 1, guests: 3 },
     { major: 'Biology', school: 'Smith', level: 'undergrad' },
     { fb: 'tinytina', ig: 'tt_ig' },
-    'Tina. Vestibulum eu facilisis risus. Integer id arcu sodales, finibus nibh nec, consequat ante. Curabitur ut arcu sollicitudin, suscipit mauris id, molestie dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sit amet nisi fermentum, fringilla enim a, aliquam lacus.',
+    `I am a hopeless romantic yet easily influenced person with a powerful sex drive and minimal social skills. I like horses, butts, zombies, boys, and writing erotic fiction about movies and my life.`,
     false, // hasHousing
     new Preferences(
-        ['Amherst', 'Sunderland', 'Northampton'],
-        { min: 600, max: 1000 },
-        { min: 1, max: 3 },
-        { allFemale: false, allMale: true, mixed: true },
-        { semester: true, year: true },
-        { rent: true },
-        { private: true },
-        { apartment: true, house: true },
-        { fall: true },
-        { laundry: true, parking: true }
+        null,
+        null,
+        null,
+        { allFemale: true, allMale: true, mixed: true },
+        { perSemester: true, monthly: true, sixMonths: true, yearly: true },
+        { rent: true, sublet: true },
+        { private: true, shared: true },
+        { dorm: true, apartment: true, house: true },
+        { fall: true, winter: true, spring: true, summer: true },
+        { aircon: true, dishwasher: true, hardwood: true, carpet: true, laundry: true, parking: true, bus: true, pets: true }
     ),
     new Housing(),
     ['user_5'], // liked
@@ -120,7 +120,7 @@ const u3 = new User(
     { clean: 3, sleep: 3, noise: 3, guests: 3 },
     { major: 'Music', school: 'Umass Amherst', level: 'undergrad' },
     { fb: 'geniefb', ig: 'genebelcher' },
-    'Gene. Vestibulum eu facilisis risus. Integer id arcu sodales, finibus nibh nec, consequat ante. Curabitur ut arcu sollicitudin, suscipit mauris id, molestie dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sit amet nisi fermentum, fringilla enim a, aliquam lacus.',
+    `I am an aspiring musician and a prankster. I maintain very close relationships with both of my parents and two sisters, Tina and Louise Belcher. Along with Tina, I am usually a pawn in schemes set up by Louise.`,
     true, // hasHousing
     new Preferences(),
     new Housing(
@@ -137,7 +137,7 @@ const u3 = new User(
         'fall',
         { dishwasher: true, carpet: true, parking: true, bus: true },
         ['https://ew.com/thmb/sOFfYRph6EmGoA_O5qJ-QuvJifM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/bobs-burgers1-db2ce51b2c0b4ad2b6b81dc2913227d9.jpg'],
-        'Gene housing notes. Vestibulum eu facilisis risus. Integer id arcu sodales, finibus nibh nec, consequat ante. Curabitur ut arcu sollicitudin, suscipit mauris id, molestie dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sit amet nisi fermentum, fringilla enim a, aliquam lacus.'
+        `Ocean Avenue Hi-Fi Emporium is a pawn shop that I frequent to play with a drum machine, much to the annoyance of the shop's owner, Dino. In "Drumforgiven" the above mentioned habit gets me banned from the store, causing Louise to seek revenge on Dino.`
     ),
     ['user_demo'], // liked
     ['user_4'], // rejected
@@ -156,19 +156,19 @@ const u4 = new User(
     { clean: 3, sleep: 3, noise: 3, guests: 2 },
     { major: 'History', school: 'Amherst College', level: 'undergrad' },
     { fb: 'lulufb', ig: 'louisebelcher' },
-    'Louise. Vestibulum eu facilisis risus. Integer id arcu sodales, finibus nibh nec, consequat ante. Curabitur ut arcu sollicitudin, suscipit mauris id, molestie dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sit amet nisi fermentum, fringilla enim a, aliquam lacus.',
+    `I am a bright (in the brain), mischievous yet manipulative, and aggressive child. My off-balance sense of humor and hunger for conflict makes me somewhat of a liability in the kitchen and often gets my family both into and/or out of tricky situations.`,
     false, // hasHousing
     new Preferences(
-        ['Amherst', 'Sunderland', 'Northampton'],
-        { min: 600, max: 1000 },
-        { min: 1, max: 3 },
-        { allFemale: false, allMale: true, mixed: true },
-        { semester: true, year: true },
-        { rent: true },
-        { private: true },
-        { apartment: true, house: true },
-        { fall: true },
-        { laundry: true, parking: true }
+        null,
+        null,
+        null,
+        { allFemale: true, allMale: true, mixed: true },
+        { perSemester: true, monthly: true, sixMonths: true, yearly: true },
+        { rent: true, sublet: true },
+        { private: true, shared: true },
+        { dorm: true, apartment: true, house: true },
+        { fall: true, winter: true, spring: true, summer: true },
+        { aircon: true, dishwasher: true, hardwood: true, carpet: true, laundry: true, parking: true, bus: true, pets: true }
     ),
     new Housing(),
     [], // liked
@@ -188,7 +188,7 @@ const u5 = new User(
     { clean: 1, sleep: 3, noise: 1, guests: 3 },
     { major: 'Dance', school: 'UMass Amherst', level: 'undergrad' },
     { fb: 'jimmyjrpesto.fb', ig: 'jj_ig' },
-    'Jimmy Jr. Vestibulum eu facilisis risus. Integer id arcu sodales, finibus nibh nec, consequat ante. Curabitur ut arcu sollicitudin, suscipit mauris id, molestie dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sit amet nisi fermentum, fringilla enim a, aliquam lacus.',
+    `I’m an eighth-grader at Wagstaff School in the same classes as Tina Belcher, who's had the longest crush on me since I made my first appearance in "Sheesh! Cab, Bob?," but unfortunately for her, I care more about dancing and my best friend, Zeke.`,
     true, // hasHousing
     new Preferences(),
     new Housing(
@@ -205,7 +205,7 @@ const u5 = new User(
         'fall',
         { aircon: true, hardwood: true, parking: true },
         [ 'https://static.wikia.nocookie.net/bobsburgerpedia/images/3/31/Jimmypestospizza.jpg/revision/latest?cb=20110902183814', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3RIJfoiz3klpLTVdeLL77AT8rMqL1m1gpV8Aor-Pl4Q&s'],
-        'Jimmy Jr. housing notes. Vestibulum eu facilisis risus. Integer id arcu sodales, finibus nibh nec, consequat ante. Curabitur ut arcu sollicitudin, suscipit mauris id, molestie dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sit amet nisi fermentum, fringilla enim a, aliquam lacus.'
+        `Jimmy Pesto's Pizzeria is an Italian themed restaurant across the street from Bob's Burgers on Ocean Avenue. It is owned by Jimmy Pesto, who rents the property from Mr. Fischoeder. I work as a busboy. The décor consists primarily of Italian motifs, such as exposed brick, pictures and flags of Italy, grapes, etc., with one wall dedicated to Jimmy Pesto's "trophies." The menu features many Italian staples as well as "Pesto's Besto Burgers" and "Pesto Coladas."`
     ),
     ['user_demo'], // liked
     [], // rejected
